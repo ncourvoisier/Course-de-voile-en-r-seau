cfba1712ff7c8b8ff5ccc5e958bf23152d32b0e0
ncourvoisier <nicoce25@hotmail.fr>
2020-11-12 18:08:04 +0100

    add physics

--- src/server/BoatWrapper.h
+++ src/server/BoatWrapper.h
@@ -4,6 +4,7 @@
 #include <gf/Shapes.h>
 #include <gf/Vector.h>
 #include "../Protocol.h"
+#include "../physics/Boat.h"
 
 namespace sail
 {
@@ -12,27 +13,19 @@ namespace sail
     {
     public:
         BoatWrapper() = default;
-
         BoatWrapper(gf::Id playerId, gf::Vector2f position);
-
         void setVelocity(gf::Vector2f velocity);
-
         gf::Vector2f getVelocity();
-
         gf::Vector2f getPosition();
-
         void setPosition(const gf::Vector2f &mPosition);
-
         void update(float dt);
-
         BoatData getBoatData();
 
     private:
         gf::Id m_playerId;
-
         gf::Vector2f m_position;
-
         gf::Vector2f m_velocity;
+        //Boat boat;
     };
 
 }
