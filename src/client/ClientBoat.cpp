00f07813c9c0e04b8590094f098f23fce26e1d5a
ncourvoisier <nicoce25@hotmail.fr>
2021-01-04 12:11:19 +0100

    affichage du nom du joueur

--- src/client/ClientBoat.cpp
+++ src/client/ClientBoat.cpp
@@ -2,6 +2,9 @@
 #include <gf/RenderTarget.h>
 #include <iostream>
 #include <gf/Sprite.h>
+#include <gf/Text.h>
+
+#include <string>
 
 #include "../Constants.h"
 
@@ -36,7 +39,6 @@ namespace sail
         gf::Vector2f position(m_x, m_y);
 
         gf::Sprite spriteBoat;
-
         spriteBoat.setTexture(m_texture);
         spriteBoat.setPosition(position);
         spriteBoat.setScale(0.2f); // TODO : ADAPT THIS
@@ -61,6 +63,41 @@ namespace sail
         spriteRudder.setAnchor(gf::Anchor::Center);
         spriteRudder.setRotation(rudder_angle);
 
+        gf::ResourceManager resources;
+        resources.addSearchDir("../resources");
+        gf::Font& font = resources.getFont("arial.ttf");
+
+        std::string name = "Nicolas";
+        gf::Text textName;
+        textName.setString(name);
+        textName.setCharacterSize(65);
+        textName.setColor(gf::Color::White);
+        textName.setParagraphWidth(800);
+        textName.setAlignment(gf::Alignment::Center);
+        textName.setFont(font);
+        textName.setAnchor(gf::Anchor::Center);
+        textName.setRotation(80.0);
+        textName.setPosition({-120.0f, 150.0f});
+
+        /*std::string info = "Speed : Sail angle : ";
+        std::ostringstream sa;
+        sa << sail_angle;
+        std::string str = sa.str();
+
+        auto res = info + str;
+
+        gf::Text textInfo;
+        textInfo.setString(res);
+        textInfo.setCharacterSize(75);
+        textName.setColor(gf::Color::White);
+        textName.setParagraphWidth(800);
+        textName.setAlignment(gf::Alignment::None);
+        textName.setFont(font);
+        textName.setAnchor(gf::Anchor::BottomRight);
+        textName.setPosition({100.0f, 150.0f});
+
+        target.draw(textInfo, states);*/
+        target.draw(textName, shipStates);
         target.draw(spriteRudder, shipStates);
         target.draw(spriteBoat, states);
         target.draw(spriteSail, shipStates);
